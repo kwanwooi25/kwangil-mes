ed2adea7e22500f9d09cedaef2e80e55e07cb518
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-04-12 12:12:34 +0900

    working on OrderList component

--- imports/views/OrdersPage/OrderList.js
+++ imports/views/OrdersPage/OrderList.js
@@ -4,6 +4,8 @@ import { AccountsData } from '../../api/accounts';
 import { ProductsData } from '../../api/products';
 import { OrdersData } from '../../api/orders';
 
+import Checkbox from '../../custom/Checkbox';
+
 export default class OrderList extends React.Component {
   /*=========================================================================
   >> props <<
@@ -24,7 +26,7 @@ export default class OrderList extends React.Component {
       isDeleteConfirmModalOpen: false,
       selectedID: '',
       selectedName: '',
-      productsCount: 0
+      ordersCount: 0
     };
 
     // this.onInputChange = this.onInputChange.bind(this);
@@ -51,14 +53,27 @@ export default class OrderList extends React.Component {
         {},
         { fields: { _id: 1, name: 1 } }
       ).fetch();
-      const productList = ProductsData.find({}, { sort: { name: 1 } }).fetch();
-      const orderList = OrdersData.find({}, { sort: { _id: 1}}).fetch();
+      const productList = ProductsData.find(
+        {},
+        {
+          fields: {
+            _id: 1,
+            accountID: 1,
+            name: 1,
+            thick: 1,
+            length: 1,
+            width: 1,
+            isPrint: 1
+          }
+        }
+      ).fetch();
+      const orderList = OrdersData.find({}, { sort: { _id: 1 } }).fetch();
 
       this.setState({
         accountList,
         productList,
         data: orderList,
-        productsCount: productList.length
+        ordersCount: orderList.length
       });
     });
 
@@ -76,6 +91,16 @@ export default class OrderList extends React.Component {
   componentWillUnmount() {
     this.databaseTracker.stop();
     this.authTracker.stop();
+  }
+
+  comma(str) {
+    str = String(str);
+    return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,');
+  }
+
+  uncomma(str) {
+    str = String(str);
+    return str.replace(/[^\d]+/g, '');
   }
 
   // onInputChange(e) {
@@ -152,106 +177,117 @@ export default class OrderList extends React.Component {
   //   }
   // }
   //
-  // getProductList(queryObj) {
-  //   return this.state.data.map(product => {
-  //     const account = this.state.accountList.find(
-  //       account => account._id === product.accountID
-  //     );
-  //     let print = "";
-  //     if (product.printFrontColorCount) {
-  //       if (product.printBackColorCount) {
-  //         print = `(전면 ${product.printFrontColorCount}도, 후면 ${
-  //           product.printBackColorCount
-  //         }도)`;
-  //       } else {
-  //         print = `(전면 ${product.printFrontColorCount}도)`;
-  //       }
-  //     }
-  //
-  //     let matchQuery = false;
-  //
-  //     if (
-  //       product.accountName &&
-  //       product.accountName.indexOf(queryObj.accountName) > -1 &&
-  //       product.name &&
-  //       product.name.indexOf(queryObj.name) > -1 &&
-  //       product.thick &&
-  //       product.thick.indexOf(queryObj.thick) > -1 &&
-  //       product.length &&
-  //       product.length.indexOf(queryObj.length) > -1 &&
-  //       product.width &&
-  //       product.width.indexOf(queryObj.width) > -1 &&
-  //       product.extColor &&
-  //       product.extColor.indexOf(queryObj.extColor) > -1 &&
-  //       ((product.printFrontColor &&
-  //         product.printFrontColor.indexOf(queryObj.printFrontColor) > -1) ||
-  //         (product.printBackColor &&
-  //           product.printBackColor.indexOf(queryObj.printBackColor) > -1))
-  //     ) {
-  //       matchQuery = true;
-  //     }
-  //
-  //     // only show product that has matching query text
-  //     if (matchQuery) {
-  //       return (
-  //         <li className="product" key={product._id} id={product._id}>
-  //           <div className="product-checkbox-container">
-  //             <Checkbox name={product._id} onInputChange={this.onInputChange} />
-  //           </div>
-  //           <div className="product-container">
-  //             <div className="product-name-container">
-  //               <span className="product-accountName">{account.name}</span>
-  //               <a className="product-name" onClick={this.onNameClick}>
-  //                 {product.name}
-  //               </a>
-  //             </div>
-  //             <div className="product-details-container">
-  //               <div className="product-size-container">
-  //                 <span className="product-size__thick">{product.thick}</span>
-  //                 <i className="fa fa-times" />
-  //                 <span className="product-size__length">{product.length}</span>
-  //                 <i className="fa fa-times" />
-  //                 <span className="product-size__width">{product.width}</span>
-  //               </div>
-  //               <div className="product-isPrint-container">
-  //                 <span className="product-isPrint">
-  //                   {product.isPrint ? `인쇄 ${print}` : "무지"}
-  //                 </span>
-  //               </div>
-  //             </div>
-  //           </div>
-  //
-  //           {this.state.isAdmin || this.state.isManager ? (
-  //             <div className="product-buttons-container">
-  //               <button
-  //                 className="button-circle product-button"
-  //                 onClick={this.onEditClick}
-  //               >
-  //                 <i className="fa fa-edit fa-lg" />
-  //                 <span>수정</span>
-  //               </button>
-  //               <button
-  //                 className="button-circle product-button"
-  //                 onClick={this.onDeleteClick}
-  //               >
-  //                 <i className="fa fa-trash fa-lg" />
-  //                 <span>삭제</span>
-  //               </button>
-  //             </div>
-  //           ) : (
-  //             undefined
-  //           )}
-  //         </li>
-  //       );
-  //     }
-  //   });
-  // }
+  getOrderList(queryObj) {
+    return this.state.data.map(({ _id, data }) => {
+      const order = data;
+      const product = this.state.productList.find(
+        product => product._id === order.productID
+      );
+      const account = this.state.accountList.find(
+        account => account._id === product.accountID
+      );
+      console.log(_id, order, product, account);
+
+      let isPrintText = '무지';
+      if (product.isPrint) {
+        isPrintText = '인쇄';
+        switch (order.plateStatus) {
+          case 'confirm':
+            isPrintText += '(확인)';
+            break;
+          case 'new':
+            isPrintText += '(신규)';
+            break;
+          case 'edit':
+            isPrintText += '(수정)';
+            break;
+        }
+      }
+      // 납기일
+      // 납기엄수/지급
+
+      let matchQuery = true;
+
+      // only show product that has matching query text
+      if (matchQuery) {
+        return (
+          <li className="order" key={_id} id={_id}>
+            <div className="order-checkbox-container">
+              <Checkbox name={_id} onInputChange={this.onInputChange} />
+            </div>
+            <div className="order-container">
+              <div className="order-orderedAt-container">
+                <span className="order-orderedAt">{order.orderedAt}</span>
+              </div>
+              <div className="order-names-container">
+                <a className="order-accountName">{account.name}</a>
+                <a className="order-productName">{product.name}</a>
+              </div>
+              <div className="order-product-details-container">
+                <div className="order-product-size-container">
+                  <span className="order-product-size__thick">
+                    {product.thick}
+                  </span>
+                  <i className="fa fa-times" />
+                  <span className="order-product-size__length">
+                    {product.length}
+                  </span>
+                  <i className="fa fa-times" />
+                  <span className="order-product-size__width">
+                    {product.width}
+                  </span>
+                </div>
+                <div className="order-product-isPrint-container">
+                  <span className="order-product-isPrint">{isPrintText}</span>
+                </div>
+              </div>
+              <div className="order-details-container">
+                <span className="order-orderQuantity">
+                  {this.comma(order.orderQuantity) + '매'}
+                </span>
+                <span className="order-deliverBefore">{order.deliverBefore}</span>
+                <span className="order-deliverIcons">
+                  {order.deliverFast ? (
+                    <i className="fa fa-shipping-fast" />
+                  ) : order.deliverDateStrict ? (
+                    <i className="fa fa-exclamation-circle" />
+                  ) : (
+                    undefined
+                  )}
+                </span>
+              </div>
+            </div>
+
+            {/* {this.state.isAdmin || this.state.isManager ? (
+              <div className="order-buttons-container">
+                <button
+              className="button-circle order-button"
+              onClick={this.onEditClick}
+                >
+              <i className="fa fa-edit fa-lg" />
+              <span>수정</span>
+                </button>
+                <button
+              className="button-circle order-button"
+              onClick={this.onDeleteClick}
+                >
+              <i className="fa fa-trash fa-lg" />
+              <span>삭제</span>
+                </button>
+              </div>
+              ) : (
+              undefined
+            )} */}
+          </li>
+        );
+      }
+    });
+  }
 
   render() {
-    console.log(this.state.data);
     return (
       <ul id="order-list">
-        {/* {this.state.productsCount &&
+        {/* {this.state.ordersCount &&
           (this.state.isAdmin || this.state.isManager) ? (
             <div className="product-select-all">
           <Checkbox
@@ -264,7 +300,7 @@ export default class OrderList extends React.Component {
             undefined
         )} */}
 
-        {/* {this.getProductList(this.state.queryObj)} */}
+        {this.getOrderList(this.state.queryObj)}
         {/* {this.state.isDetailViewOpen ? (
           <ProductDetailView
             isOpen={this.state.isDetailViewOpen}
